615349ed51589204ade8ec3ba719baa31517a1c4
Jean Tampon <jean.tampon@gmail.com>
2020-12-07 17:02:55 +0100

    Update 07.py

--- 07.py
+++ 07.py
@@ -4,25 +4,22 @@ def loadData(day):
 def parseRule(line):
     # Remove the dot
     parts = line.strip('\n')[:-1].split('bags contain')
-    name = parts[0].strip()
     subs = []
     if parts[1].find("no other bags") == -1:
         for s in parts[1].split(','):
             count, bag = s.strip().split(' ', 1)
             subs.append((int(count), bag[:bag.rfind(' ')]))
-    return name, subs
+    return parts[0].strip(), subs
 
 def canContain(outer, inner, rules):
-    rule = rules.get(outer, [])
-    return sum(b == inner or canContain(b, inner, rules) for _, b in rule)
+    return sum(b == inner or canContain(b, inner, rules) for _, b in rules.get(outer, [])) > 0
 
 def count(bag, rules):
-    rule = rules.get(bag, None)
-    return sum(c * (count(b, rules) + 1) for c, b in rule)
+    return sum(c * (count(b, rules) + 1) for c, b in rules.get(bag, []))
 
 def solve1():
     rules = dict(parseRule(l) for l in loadData('07'))
-    return sum(canContain(r, "shiny gold", rules) > 0 for r in rules.keys())
+    return sum(canContain(r, "shiny gold", rules) for r in rules.keys())
 
 def solve2():
     rules = dict(parseRule(l) for l in loadData('07'))
