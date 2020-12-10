0b42f8364f061e96f41b3bb73000e44983ae8e19
Jean Tampon <jean.tampon@gmail.com>
2020-12-10 16:14:53 +0100

    Update solve.py

--- 10/solve.py
+++ 10/solve.py
@@ -1,12 +1,12 @@
-def solve_1(data):
-    offsets = [data[i+1] - m for i, m in enumerate(data[:-1])] + [3]
-    return offsets.count(1) * offsets.count(3)
+import math
 
-def solve_2(idx, d, t):
-    if t - d[idx] < 4:
-        return 1
-    return sum([solve_2(idx+i, d, t) for i in range(1, 4) if (idx + i < len(d)) and (d[idx + i] - d[idx] < 4)])
-
+def solve_2_lol(offsets):
+    oc = [len(s) for s in ''.join([str(i) for i in offsets]).split('3') if len(s) > 1]
+    return math.prod([[2, 4, 7][i-2] for i in oc])
+    
 data = [0] + sorted([int(l) for l in open('input')])
-print(solve_1(data))
-print(solve_2(0, data, max(data) + 3))
+offsets = [data[i+1] - m for i, m in enumerate(data[:-1])] + [3]
+# Problem 1
+print(offsets.count(1) * offsets.count(3))
+# Problem 2
+print(solve_2_lol(offsets))
