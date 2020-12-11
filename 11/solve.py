acc53fb4509e708e9686a763cabad0ad64f6fcea
Jean Tampon <jean.tampon@gmail.com>
2020-12-11 12:28:20 +0100

    Update solve.py

--- 11/solve.py
+++ 11/solve.py
@@ -3,30 +3,47 @@ from copy import deepcopy
 def valid(row, col, grid):
     return len(grid) > row > -1 and len(grid[0]) > col > -1
 
-def get_count(row, col, grid):
-    offsets = [(row + i, col + j) for i in range(-1, 2) for j in range(-1, 2) if i != 0 or j != 0]
-    return sum([grid[i][j] == '#' for i, j in offsets if valid(i, j, grid)])
+def get_sight(row, col, grid, direction, mxd):
+    r, c, d = row + direction[0], col + direction[1], 1
+    while valid(r, c, grid) and d <= mxd:
+        if grid[r][c] == "#":
+            return 1
+        if grid[r][c] == "L":
+            return 0
+        r, c, d = r + direction[0], c + direction[1], d + 1
+    return 0
 
-def next_state(current, occupied):
+def get_count(row, col, grid, max_dist):
+    offsets = [(i, j) for i in range(-1, 2) for j in range(-1, 2) if i != 0 or j != 0]
+    return sum([get_sight(row, col, grid, (i, j), max_dist) for i, j in offsets])
+
+def next_state(current, occupied, max_seats):
     if current == 'L' and occupied == 0:
         return '#'
-    if current == '#' and occupied > 3:
+    if current == '#' and occupied > max_seats:
         return 'L'
     return current
 
-def simulate(grid):
-    next_grid = deepcopy(grid)
-    for i, row in enumerate(grid):
-        for j, col in enumerate(row):
-            next_grid[i][j] = next_state(col, get_count(i, j, grid))
-    return next_grid
+def simulate_1(grid_1, grid_2, max_dist, max_seats):
+    for r, row in enumerate(grid_1):
+        for c, col in enumerate(row):
+            grid_2[r][c] = next_state(col, get_count(r, c, grid_1, max_dist), max_seats)
+    return grid_1, grid_2
 
-def solve_1(grid):
-    next_grid = simulate(grid)
+def solve(grid, max_dist, max_seats):
+    next_grid = deepcopy(grid)
+    grid, next_grid = simulate_1(grid, next_grid, max_dist, max_seats)
+    for r in next_grid:
+        print(''.join(r))
+    print()
     while grid != next_grid:
-        grid = next_grid
-        next_grid = simulate(grid)
+        grid, next_grid = simulate_1(next_grid, grid, max_dist, max_seats)
+        for r in next_grid:
+            print(''.join(r))
+        print()
     return sum([row.count('#') for row in grid])
 
+
 data = [list(l.strip('\n')) for l in open('input')]
-print(solve_1(deepcopy(data)))
+#print(solve(deepcopy(data), 1, 3))
+print(solve(deepcopy(data), max(len(data), len(data[0])), 4))
