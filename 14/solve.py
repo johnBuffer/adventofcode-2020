d4a93e27cc12f26dd26ed2e73d08c0177c3a7cf6
Jean Tampon <jean.tampon@gmail.com>
2020-12-14 11:34:35 +0100

    Update solve.py

--- 14/solve.py
+++ 14/solve.py
@@ -5,15 +5,6 @@ def parse(line):
     else:
         return int(left[left.find('[') + 1 : left.find(']')]), int(right), None
 
-def solve_1(data):
-    mask, memory = 'X'*36, {}
-    for addr, val, msk in data:
-        mask = msk if msk else mask
-        if addr:
-            val_bits = [(val//(2**(35 - i)))%2 if mask[i] == 'X' else int(mask[i]) for i in range(36)]
-            memory[addr] = sum([b * (2**(35 - i)) for i, b in enumerate(val_bits)])
-    return sum(v for v in memory.values())
-
 def write_mem(address, value, mask, index, memory):
     if index == len(mask):
         memory[address] = value
@@ -22,6 +13,15 @@ def write_mem(address, value, mask, index, memory):
         write_mem(address | bitmask, value, mask, index + 1, memory)
         if mask[index] == 'X':
             write_mem((address | bitmask) ^ bitmask, value, mask, index + 1, memory)
+
+def solve_1(data):
+    mask, memory = 'X'*36, {}
+    for addr, val, msk in data:
+        mask = msk if msk else mask
+        if addr:
+            val_bits = [(val//(2**(35 - i)))%2 if mask[i] == 'X' else int(mask[i]) for i in range(36)]
+            memory[addr] = sum([b * (2**(35 - i)) for i, b in enumerate(val_bits)])
+    return sum(v for v in memory.values())
 
 def solve_2(data):
     mask, memory = '0'*36, {}
