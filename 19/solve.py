db8cf1e32f8debfbd045c8b1ac8250869d1f3bb1
johnBuffer <jean.tampon@gmail.com>
2020-12-19 13:34:29 +0100

    Add 19

---
+++ 19/solve.py
@@ -0,0 +1,44 @@
+import re
+
+def load_data():
+    rules_data, words_data = open('input').read().split('\n\n')
+    return parse_rules(rules_data), [w for w in words_data.split('\n')]
+
+def parse_rules(data):
+    rules = {}
+    for l in data.split('\n'):
+        left, right = l.split(': ')
+        if right[0] == '"': rules[left] = right[1], None, None
+        else:
+            if right.find(' | ') > -1:
+                or_left, or_right = right.split(' | ')
+                rules[left] = None, or_left.split(' '), or_right.split(' ')
+            else:
+                rules[left] = None, right.split(' '), None
+    return rules
+
+def build_rule_rec(rule, all_rules, deep=0):
+    if deep < 20: # my humble solution for part 2 xD
+        char, left, right = all_rules[rule]
+        if char is not None: 
+            return char
+
+        reg_left = ''.join([build_rule_rec(r,  all_rules, deep + 1) for r in left])
+        reg_right = '' if not right else ''.join([build_rule_rec(r, all_rules, deep + 1) for r in right])
+
+        return '({})'.format('|'.join(['(' + r + ')' for r in [reg_left, reg_right] if len(r)]))
+    return ''
+
+def build_rule(rule, all_rules):
+    return '^{}$'.format(build_rule_rec(rule, all_rules))
+
+def match_rule(word, rule, all_rules):
+    char, left, right = rule
+    if char is not None:
+        return word[0] == char, word[1:]
+
+rules, words = load_data()
+regex_data = build_rule('0', rules)
+# Part 1 and 2
+regex = re.compile(regex_data)
+print(sum(bool(regex.match(w)) for w in words))
